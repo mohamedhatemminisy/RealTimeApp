82654cd00ef51bdd954c127fc9d6304bb7ef159f
mohamedhatemminisy <mohamed.hatem100100@gmail.com>
2021-10-16 23:30:02 +0200

    model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Replay;
+use App\Models\Category;
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function replays(){
+        return $this->hasMany(Replay::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Replay.php
+++ app/Models/Replay.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
+use App\Models\User;
+use App\Models\Like;
 
 class Replay extends Model
 {
     use HasFactory;
+
+    public function question(){
+        $this->belongsTo(Question::class);
+    }
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
+
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
